75f235043c0eec9ae1b88fa060fb70360684dfb0
Azael Reyes-Morales <azaelreyes-morales@Azaels-MacBook-Pro.local>
2023-04-26 10:30:18 -0700

    connected to mongodb, added an env file as well.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const port = 5500;
+require('dotenv').config();
+const port = process.env.PORT || 5500;
+const connectDB = require('./config/db')
+
+connectDB();
 
 const app =  express();
 
