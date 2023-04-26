89f85964628ec18882364888ebe843ca9f24236e
Azael Reyes-Morales <azaelreyes-morales@Azaels-MacBook-Pro.local>
2023-04-26 09:06:04 -0700

    added ability to updand ideas using put and delete requests

--- routes/ideas.js
+++ routes/ideas.js
@@ -36,8 +36,46 @@ router.get('/:id', (req,res)=>{
         .status(404)
         .json({succes:false, error:'Resource not found'})
     }
-
-    res.send({succes: true, data: idea});
+    res.json({succes: true, data: idea});
 });
+//Add an idea
+router.post('/', (req,res)=>{
+  const idea = {
+    id:ideas.length+1, 
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0,10)
+  }
+  ideas.push(idea)
+
+  res.json({succes:true, data:idea});
+});
+//Update Idea
+router.put('/:id', (req,res)=>{
+  const idea = ideas.find((idea)=>idea.id ===+req.params.id)
+  if(!idea){
+      return res
+      .status(404)
+      .json({succes:false, error:'Resource not found'})
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({succes: true, data: idea});
+});
+//Delete an Idea
+router.delete('/:id',(req,res)=>{
+  const idea = ideas.find((idea)=>idea.id==+req.params.id)
+  if(!idea){
+    return res
+    .status(404)
+    .json({succes:false, error:'Resource not found'})
+  }
+  const index = ideas.indexOf(idea);
+  ideas.splice(index,1);
+  res.json({succes:true, data: {}})
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = 5500;
 
 const app =  express();
 
+//Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.get('/', (req,res)=>{
     res.send({messsege: 'Welcome to the RandomIdeas API'});
 });
